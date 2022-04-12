d7fb8cd81903e1a67185f44665dcf70a8265424a
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2022-04-13 00:27:07 +0530

    infinite scrolling

---
+++ InfiniteScrolling/script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('.container');
+let limit = 4;
+let pageCount = 1;
+let postCount = 1;
+const getPost = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${pageCount}`)
+    const posts = await res.json();
+    console.log(posts);
+
+    posts.map((curElem,index)=>{
+        const htmlData = `
+        <div class="posts">
+        <p class="post-id">${postCount++}</p>
+        <h2 class="title">${curElem.title}</h2>
+        <p class="post-info">${curElem.body}</p>
+        </div>
+        `
+        container.insertAdjacentHTML('beforeend', htmlData);
+    })
+}
+getPost();
+
+const showData = () => {
+    setTimeout(() =>{
+       pageCount++;
+       getPost(); 
+    },300)
+}
+window.addEventListener('scroll', ()=>{
+    const {scrollHeight,clientHeight,scrollTop} = document.documentElement;
+    if(scrollHeight - scrollTop <= clientHeight + 50){
+    // if(scrollHeight-scrollTop < clientHeight-0.3*clientHeight  ){
+        console.log('I am at bottom');
+        showData();
+    }
+})
